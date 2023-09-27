b2f0788a988a133bea4d6a08ca8fc409a5a16e51
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-27 17:38:14 -0600

    Finished: JournalLayout

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,9 +1,23 @@
+import { Box, Toolbar } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import { Navbar, Sidebar } from '../components';
+
+const drawerWidth = 240;
 
 export const JournalLayout = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    <Box sx={{ display: 'flex' }}>
+
+      <Navbar drawerWidth={drawerWidth} />
+      <Sidebar drawerWidth={drawerWidth} />
+
+      <Box
+        component='main'
+        sx={{ flexGrow: 1, p: 3 }}
+      >
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
   )
 }
