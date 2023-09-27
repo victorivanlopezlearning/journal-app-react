444b5f2daa7ed58a31ee46dcf40df34db04eec55
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-27 14:17:43 -0600

    Finished: Auth Layout

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -1,9 +1,32 @@
+import { Grid, Typography } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 export const AuthLayout = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: 'primary.main',
+        padding: 2
+      }}
+    >
+      <Grid
+        item
+        className="box-shadow"
+        sx={{
+          width: { sm: 500 },
+          backgroundColor: 'white',
+          padding: 4,
+          borderRadius: 3
+        }}
+      >
+        <Outlet />
+      </Grid>
+    </Grid>
   )
 }
