a23497616664c85792aa44a94762222f38acb8d7
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-27 20:24:49 -0600

    Added: button floating

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,26 @@
-import { NothingSelectedView , NoteView } from "../views"
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+import { NothingSelectedView, NoteView } from '../views';
 
 export const JournalPage = () => {
   return (
-    // <NothingSelectedView />
-    <NoteView />
+    <>
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+    </>
   )
 }
