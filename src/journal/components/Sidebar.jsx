80832fa9b77ef81dac1ead10dd30882ca7fcfcb9
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-04 19:28:28 -0600

    Added: displayName

--- src/journal/components/Sidebar.jsx
+++ src/journal/components/Sidebar.jsx
@@ -1,7 +1,11 @@
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 import { TurnedInNot } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
 
 export const Sidebar = ({ drawerWidth = 240 }) => {
+
+  const { displayName } = useSelector(state => state.auth);
+
   return (
     <Box
       component='nav'
@@ -21,7 +25,7 @@ export const Sidebar = ({ drawerWidth = 240 }) => {
             component='p'
             noWrap
           >
-            Víctor Iván López
+            {displayName}
           </Typography>
         </Toolbar>
         <Divider />
