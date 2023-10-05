07082cb0879cbb7c4743305292f17dce81b9376c
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-04 18:17:48 -0600

    Added: PublicRoute and PrivateRoute

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,16 +2,24 @@ import { AuthLayout } from '../auth/layout/AuthLayout';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalLayout } from '../journal/layout/JournalLayout';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = [
   {
     path: 'auth',
-    element: <AuthLayout />,
+    element: 
+    <PublicRoute>
+      <AuthLayout />
+    </PublicRoute>,
     children: AuthRoutes,
   },
   {
     path: '/',
-    element: <JournalLayout />,
+    element:
+    <PrivateRoute>
+      <JournalLayout />
+    </PrivateRoute>,
     children: JournalRoutes,
   },
 ]
