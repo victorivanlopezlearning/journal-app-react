22c2a1d4dd75f5fe60349803d0d674615c6d08ed
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-04 18:17:04 -0600

    Created: PrivateRoute

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
+export const PrivateRoute = ({ children }) => {
+
+  const { status } = useSelector(state => state.auth);
+
+  return (status === 'authenticated')
+    ? children
+    : <Navigate to="/auth/login" />
+}
