f55bf33c66d9bd9c52bfd6fc2591e181310f404d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-04 18:27:43 -0600

    Created: useCheckAuth custom hook

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -4,14 +4,22 @@ import {
 } from 'react-router-dom';
 import { AppRouter } from './router/AppRouter';
 import { AppTheme } from './theme';
+import { CheckingAuth } from './ui/components/CheckingAuth';
+import { useCheckAuth } from './hooks';
 
 const router = createBrowserRouter(AppRouter);
 
 export const JournalApp = () => {
-  
+
+ const status = useCheckAuth();
+
   return (
     <AppTheme>
-      <RouterProvider router={router} />
+      {
+        (status === 'checking')
+          ? <CheckingAuth />
+          : <RouterProvider router={router} />
+      }
     </AppTheme>
   )
 }
