d9c7c95a5032eef3ccba9fe1ec5e5d185bcc8683
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-13 13:18:18 -0600

    Updated: functionality

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { onAuthStateChanged } from 'firebase/auth';
 import { FirebaseAuth } from '../firebase/config';
 import { login, logout } from '../store/auth';
+import { startLoadingNotes } from '../store/journal';
 
 export const useCheckAuth = () => {
 
@@ -13,9 +14,10 @@ export const useCheckAuth = () => {
     onAuthStateChanged(FirebaseAuth, async (user) => {
 
       if (!user) return dispatch(logout());
-      
+
       const { uid, email, displayName, photoURL } = user;
       dispatch(login({ uid, email, displayName, photoURL }));
+      dispatch(startLoadingNotes());
     });
   }, [])
 
