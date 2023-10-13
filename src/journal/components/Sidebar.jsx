caac09cc23d8991c9bd9e3d63189d10be2df6606
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-13 15:41:44 -0600

    Added: SidebarItem component child

--- src/journal/components/Sidebar.jsx
+++ src/journal/components/Sidebar.jsx
@@ -1,10 +1,11 @@
-import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
-import { TurnedInNot } from '@mui/icons-material';
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { SidebarItem } from './';
 
 export const Sidebar = ({ drawerWidth = 240 }) => {
 
   const { displayName } = useSelector(state => state.auth);
+  const { notes } = useSelector(state => state.journal);
 
   return (
     <Box
@@ -32,18 +33,8 @@ export const Sidebar = ({ drawerWidth = 240 }) => {
 
         <List>
           {
-            ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <TurnedInNot />
-                  </ListItemIcon>
-                  <Grid container>
-                    <ListItemText primary={text} />
-                    <ListItemText secondary={'Lorem ipsum dolor sit amet.'} />
-                  </Grid>
-                </ListItemButton>
-              </ListItem>
+            notes.map(note => (
+              <SidebarItem key={note.id} note={note} />
             ))
           }
         </List>
