0e70f056554941908562bc3cd084fd1fff37fcb7
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-16 16:33:46 -0600

    Created: startSaveNote functionality

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,13 @@
 import { collection, doc, setDoc } from 'firebase/firestore/lite';
 import { FirebaseDB } from '../../firebase/config';
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from './journalSlice';
+import { 
+  addNewEmptyNote, 
+  savingNewNote, 
+  setActiveNote, 
+  setNotes, 
+  setSaving, 
+  updateNote
+} from './journalSlice';
 import { loadNotes } from '../../journal/helpers';
 
 export const startNewNote = () => {
@@ -34,5 +41,22 @@ export const startLoadingNotes = () => {
 
     const notes = await loadNotes(uid);
     dispatch(setNotes(notes));
+  }
+}
+
+export const startSaveNote = () => {
+  return async (dispatch, getState) => {
+
+    dispatch(setSaving());
+
+    const { uid } = getState().auth;
+    const { active: note } = getState().journal;
+
+    const noteToFireStore = { ...note };
+    delete noteToFireStore.id;
+
+    const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
+    await setDoc(docRef, noteToFireStore, { merge: true }); // Update
+    dispatch(updateNote(note));
   }
 }
